73b4e476c28c62320ad17491a294519f7d5c3a03
Maximilian Ruta <mr@deltachaos.de>
2025-03-21 18:35:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, WebDriverException
 
 def get_interval(name, default=24):
     env_value = os.getenv(name, "").strip()
